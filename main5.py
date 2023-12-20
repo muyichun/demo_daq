2b58e696d482c64deea39922df814071c0d11671
xujing <289651938@qq.com>
2023-12-21 02:36:36 +0800

    åfix ttl

---
+++ main5.py
@@ -0,0 +1,38 @@
+import nidaqmx
+from pylab import *
+from matplotlib.animation import FuncAnimation
+plt.rcParams["font.sans-serif"] = ["SimHei"]  # 设置字体
+plt.rcParams["axes.unicode_minus"] = False  # 正常显示负号
+
+# 定义初始常量
+COLLECTION_QUANTITY = 5000
+x_axis_time = [i for i in range(COLLECTION_QUANTITY)]
+y_axis_amplitude = np.zeros(COLLECTION_QUANTITY)
+
+# 画布基本信息
+fig = plt.figure(figsize=(12.8, 7.2))
+# ax = plt.subplots()
+line, = plt.plot(x_axis_time, y_axis_amplitude)
+plt.ylim((-1.2, 1.2))
+plt.yticks([-1.2,-1.0,-0.8,-0.6,-0.4,-0.2,0,0.2,0.4,0.6,0.8,1.0,1.2])
+plt.xlabel('Time')
+plt.ylabel('Amplitude')
+plt.title("daq采集信号")
+
+
+# 更新函数，每次调用更新 y 数据
+def update(frame, arg1):
+    # 读取NI_DAQMX
+    with nidaqmx.Task() as task:
+        # 选择指定串口
+        task.ai_channels.add_ai_voltage_chan("Dev1/ai0")
+        # 实时采集并绘图采集点
+        y_axis_amplitude_change = np.round(task.read(number_of_samples_per_channel=COLLECTION_QUANTITY), 5)
+        line.set_ydata(y_axis_amplitude_change)
+        return line,
+
+
+# 设置动画
+ani = FuncAnimation(fig, update, fargs=(3,), interval=100, cache_frame_data=False, repeat=False, blit=True)
+# 展示界面
+plt.show()
---
+++ main6.py
@@ -0,0 +1,14 @@
+# This is a sample Python script.
+from pylab import *
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def print_hi(name):
+    x_axis_time = np.empty(500)
+    for i in range(0,500):
+        x_axis_time[i] = i
+    print((x_axis_time))
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
