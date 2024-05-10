a903da22e671ac909dabc900fb84ed7efb187369
xujing <289651938@qq.com>
2024-05-11 00:51:18 +0800

    xxx

--- demo.py
+++ demo.py
@@ -8,108 +8,112 @@ import nidaqmx
 from nidaqmx.constants import Edge,AcquisitionType
 import threading
 import time
+import h5py
+import os
 plt.rcParams["font.sans-serif"] = ["SimHei"]  # 设置字体
 plt.rcParams["axes.unicode_minus"] = False  # 正常显示负号
 __author__ = "Jing Xu"
 
 '''
-2个波形用线程展示，暂时解决不了！
+改成1个波形，用子线程更新展示
 '''
 class WaveformGUI:
     def update_waveform1(self):
-        while True:
-            # 更新数据（这里仅作示例，实际应根据你的需求获取实时数据）
-            with nidaqmx.Task("task") as task:
-                # 选择指定串口
-                task.ai_channels.add_ai_voltage_chan("Dev1/ai0","ch1")
-                # 选择时钟同步串口
-                task.timing.cfg_samp_clk_timing(2E+6, "", TRIGGER_EDGE, AcquisitionType.CONTINUOUS)
-                task.triggers.start_trigger.cfg_dig_edge_start_trig("/Dev1/PFI0", TRIGGER_EDGE)
-                # 实时采集并绘图采集点
-                y_axis_amplitude_change1 = np.round(task.read(number_of_samples_per_channel=COLLECTION_QUANTITY), 5)
-                # 在主线程中更新图形以避免与Tkinter GUI冲突
-                self.master.after(100, self.redraw1(y_axis_amplitude_change1))
+        # 是否把数据导出h5文件
+        if self.s_b.cget('text') == 'Deny':
+            # 判断是覆盖 or 追加，如果是覆盖，直接删除文件
+            if self.write_mode.get() == 'Overwrite' and os.path.exists(self.h5_input.get()):
+                os.remove(self.h5_input.get())
+            with h5py.File(self.h5_input.get(), 'a') as file:
+                dataset = file.create_dataset(
+                    'Imaging data_' + time.strftime("%Y_%m_%d_%H_%M_%S", time.localtime())
+                    , shape=(0, self.collection_quantity)
+                    , maxshape=(None, self.collection_quantity)
+                    , dtype=np.float32
+                    , chunks=self.chunk_size)
+                # 初始化创建字符串属性
+                dataset.attrs['Display Time'] = 0
+                # 实时更新波形
+                while not self.stop_event.is_set():
+                    # 更新数据（这里仅作示例，实际应根据你的需求获取实时数据）
+                    with nidaqmx.Task() as task:
+                        # 选择指定串口
+                        task.ai_channels.add_ai_voltage_chan(self.p_c_i1.get())
+                        # 选择时钟同步串口
+                        active_edge = TRIGGER_EDGE[self.s_e_i['values'].index(self.s_e_i.get())]
+                        task.timing.cfg_samp_clk_timing(2E+6, "", active_edge, AcquisitionType.CONTINUOUS)
+                        task.triggers.start_trigger.cfg_dig_edge_start_trig(self.s_t_s_i.get(), active_edge)
+                        # 实时采集并绘图采集点
+                        y_axis_amplitude_change1 = np.round(task.read(number_of_samples_per_channel=self.collection_quantity), 5)
+                        # 追加写入新数据
+                        current_len = len(dataset)
+                        # print("写入中：", current_len)
+                        dataset.resize(current_len + 1, axis=0)
+                        dataset[current_len:] = y_axis_amplitude_change1
+                        # 在主线程中更新图形以避免与Tkinter GUI冲突
+                        self.master.after(0, self.redraw1(y_axis_amplitude_change1))
+        else:
+            # 实时更新波形
+            while not self.stop_event.is_set():
+                # 更新数据（这里仅作示例，实际应根据你的需求获取实时数据）
+                with nidaqmx.Task() as task:
+                    # 选择指定串口
+                    task.ai_channels.add_ai_voltage_chan(self.p_c_i1.get())
+                    # 选择时钟同步串口
+                    active_edge = TRIGGER_EDGE[self.s_e_i['values'].index(self.s_e_i.get())]
+                    task.timing.cfg_samp_clk_timing(2E+6, "", active_edge, AcquisitionType.CONTINUOUS)
+                    task.triggers.start_trigger.cfg_dig_edge_start_trig(self.s_t_s_i.get(), active_edge)
+                    # 实时采集并绘图采集点
+                    y_axis_amplitude_change1 = np.round(task.read(number_of_samples_per_channel=self.collection_quantity), 5)
+                    # 在主线程中更新图形以避免与Tkinter GUI冲突
+                    self.master.after(0, self.redraw1(y_axis_amplitude_change1))
+        # stop清零复原
+        self.master.after(0, self.redraw1(np.zeros(self.collection_quantity)))
 
-    def update_waveform2(self):
-        while True:
-            # 更新数据（这里仅作示例，实际应根据你的需求获取实时数据）
-            with nidaqmx.Task("task2") as task2:
-                # 选择指定串口
-                task2.ai_channels.add_ai_voltage_chan("Dev1/ai1", "ch2")
-                # 选择时钟同步串口
-                task2.timing.cfg_samp_clk_timing(2E+6, "", TRIGGER_EDGE, AcquisitionType.CONTINUOUS)
-                # task2.triggers.start_trigger.cfg_dig_edge_start_trig("/Dev1/PFI0", TRIGGER_EDGE)
-                # 实时采集并绘图采集点
-                y_axis_amplitude_change2 = np.round(task2.read(number_of_samples_per_channel=COLLECTION_QUANTITY), 5)
-                # 在主线程中更新图形以避免与Tkinter GUI冲突
-                self.master.after(100, self.redraw2(y_axis_amplitude_change2))
 
     def redraw1(self, y_axis_amplitude_change1):
         self.line1.set_ydata(y_axis_amplitude_change1)
-        self.canvas.draw_idle()  # 绘制新的图形
-
-    def redraw2(self, y_axis_amplitude_change2):
-        self.line2.set_ydata(y_axis_amplitude_change2)
         self.canvas.draw_idle()  # 绘制新的图形
 
 
     def __init__(self, master):
         self.master = master
         self.master.title('daq采集信号')
+        self.collection_quantity = COLLECTION_QUANTITY
+        self.chunk_size = (1, COLLECTION_QUANTITY)
 
-        # 1.Create a frame for matplotlib canvas
-        fig_frame = tk.Frame(master)
+        # 1. 创建 a frame for matplotlib canvas
+        fig_frame = tk.Frame(self.master)
         fig_frame.pack(side=tk.LEFT, fill=tk.BOTH, expand=1)
 
-        # Generate sample data and create figure
         x1 = [i for i in range(COLLECTION_QUANTITY)]
         y1 = np.zeros(COLLECTION_QUANTITY)
-        x2 = [i for i in range(COLLECTION_QUANTITY)]
-        y2 = np.zeros(COLLECTION_QUANTITY)
 
         self.fig = plt.figure(figsize=(10.8, 7.2), dpi=100)
-        self.ax1 = self.fig.add_subplot(211)
-        self.ax2 = self.fig.add_subplot(212)
+        self.ax1 = self.fig.add_subplot(111)
         self.line1, = self.ax1.plot(x1, y1, color='blue', linewidth=2)
-        self.line2, = self.ax2.plot(x2, y2, color='red', linewidth=2)
         # Set axis labels and limits
         self.ax1.set_xlabel('Time')
         self.ax1.set_ylabel('Amplitude')
-        self.ax1.set_ylim((-1.2, 1.2))
-        self.ax1.set_yticks([-1.2, -1.0, -0.8, -0.6, -0.4, -0.2, 0, 0.2, 0.4, 0.6, 0.8, 1.0, 1.2])
-
-        self.ax2.set_xlabel('Time')
-        self.ax2.set_ylabel('Amplitude')
-        self.ax2.set_ylim((-1.2, 1.2))
-        self.ax2.set_yticks([-1.2, -1.0, -0.8, -0.6, -0.4, -0.2, 0, 0.2, 0.4, 0.6, 0.8, 1.0, 1.2])
-
+        self.ax1.set_ylim((0, 2.2))
+        self.ax1.set_yticks([0, 0.2, 0.4, 0.6, 0.8, 1.0, 1.2, 1.4, 1.6, 1.8, 2.0, 2.2])
         # Add matplotlib canvas to the frame
         self.canvas = FigureCanvasTkAgg(self.fig, master=fig_frame)
         self.canvas.draw()
         self.canvas.get_tk_widget().pack(side=tk.LEFT, fill=tk.BOTH, expand=1)
 
-        # 创建并启动更新线程
-        self.update_thread1 = threading.Thread(target=self.update_waveform1, daemon=True)
-        self.update_thread1.start()
-        self.update_thread2 = threading.Thread(target=self.update_waveform2, daemon=True)
-        self.update_thread2.start()
-
-        # 2.Create a frame for input boxes and buttons
+        # 2. Create a frame for input boxes and buttons
         control_frame = tk.Frame(master)
         control_frame.pack(side=tk.TOP, fill=tk.Y)
         separator1 = tk.Label(control_frame, text="Custom", bd=1, pady=10, relief=tk.SUNKEN, bg="gray")
         # Create four input boxes
         self.p_c_l1 = tk.Label(control_frame, text="Physical Channels 1", fg="blue")
-        self.p_c_i1 = ttk.Combobox(control_frame, values=["Dev1/ai0"],state='readonly')
+        self.p_c_i1 = ttk.Combobox(control_frame, values=["Dev1/ai0", "Dev1/ai1"], state='readonly')
         self.p_c_i1.current(0)
-
-        self.p_c_l2 = tk.Label(control_frame, text="Physical Channels 2", fg="red")
-        self.p_c_i2 = ttk.Combobox(control_frame, values=["Dev1/ai1"],state='readonly')
-        self.p_c_i2.current(0)
 
         self.s_p_c_l = tk.Label(control_frame, text="Samples Per Channel")
         self.s_p_c_i = tk.Entry(control_frame)
-        self.s_p_c_i.insert(0,"5000")
+        self.s_p_c_i.insert(0,"400")
 
         self.s_e_l = tk.Label(control_frame, text="Start Edge")
         self.s_e_i = ttk.Combobox(control_frame, values=["Rising", "Falling"],state='readonly')
@@ -124,8 +128,6 @@ class WaveformGUI:
         separator1.pack(fill=tk.X)
         self.p_c_l1.pack()
         self.p_c_i1.pack()
-        self.p_c_l2.pack()
-        self.p_c_i2.pack()
         self.s_p_c_l.pack()
         self.s_p_c_i.pack()
         self.s_e_l.pack()
@@ -136,17 +138,18 @@ class WaveformGUI:
 
         # 用一个新控件，把两个单选按钮放一行
         tmp_frame1 = tk.Frame(control_frame)
-        self.s_p_b = tk.Button(tmp_frame1, text="...", command=self.choose_h5_clicked)
-        self.s_p_i = tk.Entry(tmp_frame1)
-        self.s_p_b.pack(side=tk.LEFT)
-        self.s_p_i.pack(side=tk.LEFT)
+        self.h5_button = tk.Button(tmp_frame1, text="...", command=self.choose_h5_clicked)
+        self.h5_input = tk.Entry(tmp_frame1)
+        self.h5_input.insert(0, "D:/HGY_DATA/test_daq/xj.h5")
+        self.h5_button.pack(side=tk.LEFT)
+        self.h5_input.pack(side=tk.LEFT)
         tmp_frame1.pack()
 
         # 用一个新控件，把两个单选按钮放一行
         tmp_frame2 = tk.Frame(control_frame)
-        self.radio_mode = tk.StringVar(value="Overwrite")
-        self.option1 = tk.Radiobutton(tmp_frame2, text="Overwrite", variable=self.radio_mode, value="Overwrite")
-        self.option2 = tk.Radiobutton(tmp_frame2, text="Append", variable=self.radio_mode, value="Append")
+        self.write_mode = tk.StringVar(value="Overwrite")
+        self.option1 = tk.Radiobutton(tmp_frame2, text="Overwrite", variable=self.write_mode, value="Overwrite")
+        self.option2 = tk.Radiobutton(tmp_frame2, text="Append", variable=self.write_mode, value="Append")
         # radio_var.trace("w", on_selection_change)
         self.s_b = tk.Button(control_frame, text="Confirm",fg="green", command=self.export_confirm_clicked)
         self.option1.pack(side=tk.LEFT)
@@ -168,34 +171,55 @@ class WaveformGUI:
             filetypes=[("hdf5 Files", "*.hdf5"), ("hdf5 Files", "*.h5")]
         )
         if file_path:
-            self.s_p_i.delete(0, "end")
-            self.s_p_i.insert(0, file_path)
+            self.h5_input.delete(0, "end")
+            self.h5_input.insert(0, file_path)
 
     def export_confirm_clicked(self):
         if self.s_b.cget('text') == 'Confirm':
             self.s_b.config(text="Deny", fg="red")
             self.option1['state'] = 'disabled'
             self.option2['state'] = 'disabled'
-            self.s_p_b['state'] = 'disabled'
-            self.s_p_i['state'] = 'disabled'
+            self.h5_button['state'] = 'disabled'
+            self.h5_input['state'] = 'disabled'
         else:
             self.s_b.config(text="Confirm", fg='green')
             self.option1['state'] = 'normal'
             self.option2['state'] = 'normal'
-            self.s_p_b['state'] = 'normal'
-            self.s_p_i['state'] = 'normal'
+            self.h5_button['state'] = 'normal'
+            self.h5_input['state'] = 'normal'
 
     def start_clicked(self):
         if self.begin_b.cget('text') == 'Start':
+            # 配置Custom相关参数
             self.begin_b.config(text="Stop", fg="red")
+            self.s_b['state'] = 'disabled'
+            self.collection_quantity = int(self.s_p_c_i.get())
+            self.chunk_size = (1, self.collection_quantity)
+            x = [i for i in range(self.collection_quantity)]
+            y = np.zeros(self.collection_quantity)
+            # 重绘图形
+            self.ax1.clear()
+            self.line1, = self.ax1.plot(x, y, color='blue', linewidth=2)
+            self.ax1.set_xlabel('Time')
+            self.ax1.set_ylabel('Amplitude')
+            self.ax1.set_ylim((0, 2.2))
+            self.ax1.set_yticks([0, 0.2, 0.4, 0.6, 0.8, 1.0, 1.2, 1.4, 1.6, 1.8, 2.0, 2.2])
+            # 创建并启动更新线程
+            self.stop_event = threading.Event()
+            self.update_thread1 = threading.Thread(target=self.update_waveform1, daemon=True)
+            self.update_thread1.start()
         else:
+            self.stop_event.set()
             self.begin_b.config(text="Start", fg='green')
+            self.s_b['state'] = 'normal'
+
 
 if __name__ == '__main__':
-    # 外部配置信息
-    COLLECTION_QUANTITY = 5000
-    TRIGGER_EDGE = Edge.FALLING
-
+    # 默认参数
+    COLLECTION_QUANTITY = 400
+    CHUNK_SIZE = (1, COLLECTION_QUANTITY)
+    TRIGGER_EDGE = [Edge.RISING, Edge.FALLING]
+    # 展示GUI界面
     root = tk.Tk()
     app = WaveformGUI(root)
     root.mainloop()
--- main.py
+++ main.py
@@ -1,50 +1,221 @@
-import tkinter as tk
-from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg, NavigationToolbar2Tk
-import matplotlib.pyplot as plt
 import numpy as np
+import matplotlib.pyplot as plt
+import h5py
+import time
+import scipy.signal as signal
+from scipy import signal
+import statsmodels.api as sm
 
-class WaveformGUI:
-    def __init__(self, master):
-        self.master = master
-        self.master.title('Realtime Waveform')
+def amplitude_correction(data,mod_freq,sample_rate):
+    data_f = np.fft.rfft(data)
+    main_sine_ind = int(mod_freq*len(data)/sample_rate)
+    main_sine_f = np.zeros_like(data_f)
+    main_sine_f[0] = data_f[0]
+    main_sine_f[main_sine_ind] = data_f[main_sine_ind]
+    main_sine = np.fft.irfft(main_sine_f)
+    data_corr = data - main_sine
+    return data_corr
+def generate_carrier(data, phase0, demod_ph_carr_amp):
 
-        self.fig = plt.figure(figsize=(5, 4), dpi=100)
-        self.ax = self.fig.add_subplot(111)
-        self.line, = self.ax.plot([], [])
-
-        self.canvas = FigureCanvasTkAgg(self.fig, master=self.master)
-        self.toolbar = NavigationToolbar2Tk(self.canvas, self.master)
-        self.toolbar.update()
-
-        self.canvas.get_tk_widget().pack(side=tk.TOP, fill=tk.BOTH, expand=1)
-
-        self.x_data = []
-        self.y_data = []
-
-        self.update_plot()
-
-    def update_plot(self):
-        # Generate new data point
-        x = np.linspace(0, 2*np.pi, len(self.x_data) + 100)
-        y = np.sin(x)
-
-        self.x_data.extend(x[len(self.x_data):])
-        self.y_data.extend(y[len(self.y_data):])
-
-        # Update the line with new data
-        self.line.set_data(self.x_data, self.y_data)
-
-        # Re-scale the axes
-        self.ax.relim()
-        self.ax.autoscale_view()
-
-        # Update the canvas
-        self.canvas.draw()
-
-        # Call update_plot again after 100 milliseconds
-        self.master.after(100, self.update_plot)
+    phi0 = np.deg2rad(phase0)
+    sec_harm_amp = harm_amp
+    sec_harm_phase = np.deg2rad(harm_phase)  # rad
+    num_T = int(len(data) / mod_table_length)
+    w_T = np.linspace(0, 2 * np.pi * (mod_table_length - 1) / mod_table_length, mod_table_length)
+    argument_T = demod_ph_carr_amp * (np.sin(w_T - phi0) + sec_harm_amp * np.sin(2 * w_T - phi0 - sec_harm_phase))
+    argument = np.tile(argument_T, int(num_T))
+    return np.cos(argument) + 1j * np.sin(argument)
 
 
-root = tk.Tk()
-app = WaveformGUI(root)
-root.mainloop()
+def generate_window(data, phase0, sigma=0.0225):
+
+    t = np.linspace(0, (len(data)-1)/sample_rate, len(data))
+    T = 1 / mod_freq
+    phi0 = np.deg2rad(phase0)
+    num_T = int(len(data) / mod_table_length)
+    win = np.array([])
+    t0 = phi0 / (2 * np.pi * mod_freq)
+    win_T0 = np.exp(-0.5 * ((t[0:int(mod_table_length / 2)] - t0) / (sigma * T)) ** 2)
+    win_T1 = win_T0 + np.exp(-0.5 * ((t[0:int(mod_table_length / 2)] - t0 - T / 2) / (sigma * T)) ** 2)
+    win_T = win_T1 + np.exp(-0.5 * ((t[0:int(mod_table_length / 2)] - t0 + T / 2) / (sigma * T)) ** 2)
+    win = np.tile(win_T, int(num_T * 2))
+    return win
+
+
+def get_range_view(data, dpca_min=1, dpca_max=150, res=1, sigma=0.05, stop_flag=None):
+
+    dpca_range = np.arange(dpca_min, dpca_max, float(res))
+    dpca_amp = np.zeros_like(dpca_range)
+    window = generate_window(data, phase_shift, sigma=sigma)
+    i = 0
+    for dpca in dpca_range:     # dpca sweep
+        if stop_flag is not None:
+            if stop_flag[0]:
+                break
+        carrier = generate_carrier(data,phase_shift, dpca)
+        q = data*carrier*window
+        dpca_amp[i] = np.abs(np.sum(q)) / (len(data) / mod_table_length)
+        i += 1
+
+    return dpca_range, dpca_amp
+
+def calc_map(data, dpca_min=20, dpca_max=150, dpca_res=1, sigma=0.05,
+                 phase_delay_min=80, phase_delay_max=110, phase_delay_res=0.1, stop_flag=None):
+
+    dpca_range = np.arange(dpca_min, dpca_max, float(dpca_res))
+    phase_delay_range = np.arange(phase_delay_min, phase_delay_max, float(phase_delay_res))
+
+    map_2d = np.zeros((len(dpca_range), len(phase_delay_range)))
+    j = 0
+    for ph in phase_delay_range:    # phase delay sweep
+        if stop_flag is not None:
+            if stop_flag[0]:
+                break
+        i = 0
+        window = generate_window(data, ph, sigma=sigma)
+        for dpca in dpca_range:  # amplitude sweep
+            if stop_flag is not None:
+                if stop_flag[0]:
+                    break
+            carrier = generate_carrier(data, ph, dpca)
+            q = data * carrier * window
+            map_2d[i, j] = np.abs(np.sum(q)) / (len(data) / mod_table_length)
+            i += 1
+        j += 1
+
+    return phase_delay_range, dpca_range, map_2d
+def get_peak_positions(dpca_range=None, dpca_amp=None, threshold=10):
+
+    dpca_amp_ = np.copy(dpca_amp)
+    dpca_amp_[np.nonzero(dpca_amp_ < max(dpca_amp_) * threshold/100)] = None
+
+    dpca_map_diff = np.diff(np.log(dpca_amp_))
+    dpca_range = np.array(dpca_range) + ((dpca_range[1] - dpca_range[0]) / 2)
+
+    zero_cross_index = []
+    for i in range(len(dpca_map_diff) - 1):
+        if dpca_map_diff[i + 1] < 0 and dpca_map_diff[i] > 0:
+            zero_cross_index.append(i)
+
+    peak_position = []
+    for i in zero_cross_index:
+        peak_line = dpca_map_diff[i:i + 2]
+        peak_range = dpca_range[i:i + 2]
+        pp = find_zero_cross(peak_range, peak_line)
+        peak_position.append(pp)
+
+    return peak_position
+
+def find_zero_cross(x, y):
+    # y=kx+m
+    k = (y[1] - y[0]) / (x[1] - x[0])
+    m = y[0] - k * x[0]
+    x0 = -m / k
+    return x0
+def demodulate_phase(data, range_channel, sigma=0.0225):
+    carrier = generate_carrier(data, phase_shift, range_channel)
+    window = generate_window(data, phase_shift, sigma=sigma)
+    q = data*carrier*window
+    q_filtered = lowpass_filter(q)
+    q_filtered = average(q_filtered)
+    phase = np.unwrap(np.angle(q_filtered))
+    return phase
+
+def lowpass_filter(x):
+    cutoff_hz = mod_freq / 2
+    sos = signal.butter(3, cutoff_hz, 'low', fs=sample_rate, output='sos')
+    filtered_x = signal.sosfiltfilt(sos, x)
+    return filtered_x
+
+def average(x):
+    x_reshape = np.reshape(x, (-1, mod_table_length))
+    filtered_x = np.mean(x_reshape, axis=1)
+    return filtered_x
+
+# 1. set system parameters
+sample_rate = 2e6
+mod_freq = 5e3
+# 400
+mod_table_length = int(sample_rate/mod_freq)
+print(mod_table_length)
+# 2. 读h5文件
+data_name = r'D:\HGY_DATA\test_daq\displacement_test_1_5khz_2mm.hdf5'
+with h5py.File(data_name, 'r') as f:
+    print(list(f.keys()))
+    dataset_name = list(f.keys())
+    my_data_raw = [None] * len(dataset_name)
+    print(len(dataset_name))
+    for num_dset in range(len(dataset_name)):
+        my_data_raw [num_dset] = f.get(dataset_name[num_dset])[()]
+    f.close()
+# 3. 打印原始波形
+data_to_run = my_data_raw[0]
+test_data=data_to_run[0]
+print(data_to_run.shape)
+# plt.plot(test_data)
+# plt.show()
+# 4. amplitude_correction
+data_corr = amplitude_correction(test_data,mod_freq,sample_rate)
+# 5. set processing parameters
+harm_amp = 0
+harm_phase = 0
+phase_delay_range, dpca_range, map_values = calc_map(test_data, dpca_min=5, dpca_max=80, dpca_res=1, sigma=0.07,
+                                                         phase_delay_min=20, phase_delay_max=180, phase_delay_res=1)
+# dx = (phase_delay_range[1] - phase_delay_range[0]) / 2.
+# dy = (dpca_range[1] - dpca_range[0]) / 2.
+# extent = [phase_delay_range[0] - dx, phase_delay_range[-1] + dx, dpca_range[-1] + dy, dpca_range[0] - dy]
+# plt.imshow(map_values / np.max(map_values), extent=extent, aspect='auto')
+# plt.ylabel('Demodulation Phase Carrier Amplitude / rad')
+# plt.xlabel('Phase Delay / ...° ')
+# plt.colorbar()
+# plt.show()
+# 6.
+sum_map = np.sum(map_values, axis=0)
+# plt.plot(sum_map)
+data_y = sum_map
+# Find peaks(max).
+peak_indexes = signal.argrelextrema(data_y, np.greater)
+peak_indexes = peak_indexes[0]
+print(peak_indexes)
+print(peak_indexes.shape)
+# 7. peek line
+print(peak_indexes.shape)
+# plt.plot(sum_map)
+des_ind = peak_indexes.shape[0]//2
+# index = peak_indexes[des_ind]
+# x_ver=[index,index]
+# y_ver=[0,sum_map.max()]
+# plt.plot(x_ver,y_ver)
+# print(phase_delay_range[peak_indexes[des_ind]])
+# plt.show()
+# 8. 得到phase_shift最关键
+phase_shift = phase_delay_range[peak_indexes[des_ind]]
+amp_range, amp_val = get_range_view(data_corr,dpca_min=5, dpca_max=180, res=0.1, sigma=0.05)
+# amp_range, amp_val = get_range_view(test_data,dpca_min=5, dpca_max=180, res=0.1, sigma=0.05)
+# plt.plot(amp_range, amp_val)
+# plt.xlabel('Demodulation Phase Carrier Amplitude / rad')
+# plt.ylabel('Amplitude / a.u.')
+# plt.show()
+# 9.
+peak_positions = get_peak_positions(amp_range, amp_val, threshold=0.5)
+print('Peaks maximums are at', peak_positions)
+# 10. 计算所有数据，相位
+data_all = np.reshape(data_to_run, data_to_run.shape[0]*data_to_run.shape[1])
+data_all_corr = amplitude_correction(data_all,mod_freq,sample_rate)
+phase = demodulate_phase(data_all_corr, range_channel = peak_positions[0])
+t = np.linspace(0, len(phase)/mod_freq, len(phase))
+# plt.plot(t, phase)
+# plt.xlabel('Time / s')
+# plt.ylabel('Phase / rad')
+# plt.show()
+# 11. 最终结果
+#show in displacement
+wavelength = 1550
+n = 1.0
+path = phase*wavelength/(2*np.pi*2*n)
+# ##plot displacement in micrometer
+displacement =path/2
+plt.plot(t, displacement/1e3)
+plt.ylabel(r"Displacement [$\mu$m]")
+plt.xlabel('Time [s]')
+plt.show()
